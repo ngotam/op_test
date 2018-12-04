d668ec6d0ec0695368df92d7b505309273f7ffbe
Alpa Sanghavi <alpa.sanghavi@seagate.com>
2018-12-04 13:13:14 -0800

    added support for single instance of web driver

--- tests/suites/sample_test_suite.py
+++ tests/suites/sample_test_suite.py
@@ -14,22 +14,48 @@
 
 ####################################################################################################################################################################################
 
+
 '''
 __author__ = 'asanghavi'
 
-import unittest, time
+import unittest, time,os
+from core_framework.controller import ui_controller
+from core_framework.config import global_cfg,params
 
+import base64
 
+from core_framework import core_mac_lib
 
 
 class sample_test_suite(unittest.TestCase):
 
+    uiObj = None
+    driver = None
     def __init__(self, name='runTest'):
         unittest.TestCase.__init__(self, name)
+        self.uiObj = ui_controller.ui_controller()
+        if(not global_cfg.setup_info['status']):
+            driver = self.uiObj.get_webdriver_instance(global_cfg.platform_android_wifi)
+            global_cfg.driver_instance['driver'] = driver
+            self.driver = driver
+            global_cfg.setup_info['status'] = True
+        else:
+            self.driver = global_cfg.driver_instance['driver']
 
+    # setup your test case here
+    def setup(self):
+        pass
 
     def test_launch_toolkit(self):
-        print("Test passed...")
-        #core_mac_lib.start_macOSX_application()
-        #time.sleep(5)
-        #core_mac_lib.find_and_click_uielement_by_text("Set Up")
+        #print(self.driver)
+        #print("Test passed...")
+
+        core_mac_lib.start_macOSX_application()
+        time.sleep(5)
+        core_mac_lib.find_and_click_uielement_by_text("Set Up")
+
+
+    #clean up your test case here
+    def tear_down(self):
+        pass
+
